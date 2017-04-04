e8bd10cbb4d313e8e9c8c538facb7e7e10653261
sashpro <sashpro@mail.ru>
2017-04-04 17:57:09 +0300

    1

---
+++ playlists/go.pl
@@ -0,0 +1,7 @@
+<nodes>
+    <description>A private playlist created with the YouTube API v3</description>
+    <title>Самое интересное</title>
+    <films>
+       <name>Roza</name> 
+    </films>    
+</nodes>
