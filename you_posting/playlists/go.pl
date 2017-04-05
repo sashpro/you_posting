c9acfd89034ada1dc728407de52e1f5b366f4c66
sashpro <sashpro@mail.ru>
2017-04-05 12:35:52 +0300

    chdir name

--- you_posting/playlists/go.pl
+++
@@ -1,8 +0,0 @@
-<nodes>
-    <description>A private playlist created with the YouTube API v3</description>
-    <title>Самое интересное</title>
-    <films>
-       <name>Барбоскины</name>
-       <name>Тотошка</name> 
-    </films>    
-</nodes>
--- you_posting/playlists/go2.pl
+++
@@ -1,8 +0,0 @@
-<nodes>
-    <description>A private playlist created with the YouTube API v3</description>
-    <title>Самое страшное</title>
-    <films>
-       <name>Кошмар на улице вязов</name>
-       <name>Картошка убийца</name> 
-    </films>    
-</nodes>
