96b9b42fdfba808f65359a33a1b7beba458a3574
sashpro <sashpro@mail.ru>
2017-04-05 12:51:10 +0300

    fixes

---
+++ you_post_playlist/playlists/go.pl
@@ -0,0 +1,8 @@
+<nodes>
+    <description>A private playlist created with the YouTube API v3</description>
+    <title>Самое интересное</title>
+    <films>
+       <name>Барбоскины</name>
+       <name>Тотошка</name> 
+    </films>    
+</nodes>
---
+++ you_post_playlist/playlists/go2.pl
@@ -0,0 +1,8 @@
+<nodes>
+    <description>A private playlist created with the YouTube API v3</description>
+    <title>Самое страшное</title>
+    <films>
+       <name>Кошмар на улице вязов</name>
+       <name>Картошка убийца</name> 
+    </films>    
+</nodes>
